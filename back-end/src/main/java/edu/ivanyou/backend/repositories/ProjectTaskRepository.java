9dee95801a7f52e34ced68a003b1a92175c4679e
IvanYou98 <yuanfanyou@gmail.com>
2022-03-11 10:24:17 -0500

    create entity and repository for backlog and project task

---
+++ back-end/src/main/java/edu/ivanyou/backend/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package edu.ivanyou.backend.repositories;
+
+import edu.ivanyou.backend.model.ProjectTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends JpaRepository<ProjectTask, Long> {
+}
