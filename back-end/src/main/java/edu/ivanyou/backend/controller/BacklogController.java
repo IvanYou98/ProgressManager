a6ef7ac805492ef68f127739acefb58531559e2c
IvanYou98 <yuanfanyou@gmail.com>
2022-03-14 11:59:43 -0400

    FINISH CRUD OF PROJECT TASK

--- back-end/src/main/java/edu/ivanyou/backend/controller/BacklogController.java
+++ back-end/src/main/java/edu/ivanyou/backend/controller/BacklogController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import sun.tools.jconsole.JConsole;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -36,7 +37,31 @@ public class BacklogController {
     }
 
     @GetMapping("/{project_id}")
-    public ResponseEntity<List<ProjectTask>> getProjectTasksByProjectId (@PathVariable String project_id) {
+    public ResponseEntity<List<ProjectTask>> getProjectTasksByProjectId(@PathVariable String project_id) {
         return new ResponseEntity<>(projectTaskService.findByProjectIdentifier(project_id), HttpStatus.OK);
     }
+
+    @GetMapping("/{project_id}/{project_sequence}")
+    public ResponseEntity<ProjectTask> getProjectTask(@PathVariable String project_id, @PathVariable String project_sequence) {
+        return new ResponseEntity<>(projectTaskService.getProjectTask(project_id, project_sequence), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{project_id}/{project_sequence}")
+    public ResponseEntity<?> updateProjectTask(@Valid @RequestBody ProjectTask projectTask,
+                                               BindingResult result,
+                                               @PathVariable String project_id,
+                                               @PathVariable String project_sequence) {
+        ResponseEntity<?> errorMap = validationErrorMapService.mapValidationError(result);
+        if (errorMap != null) return errorMap;
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask, project_id, project_sequence);
+        return new ResponseEntity<>(updatedTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{project_id}/{project_sequence}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String project_id,
+                                               @PathVariable String project_sequence) {
+        ProjectTask deletedTask = projectTaskService.deleteByProjectSequence(project_id, project_sequence);
+        return new ResponseEntity<String>("Project task: " + project_sequence + " is deleted." , HttpStatus.OK);
+    }
 }
+
--- back-end/src/main/java/edu/ivanyou/backend/services/ProjectTaskService.java
+++ back-end/src/main/java/edu/ivanyou/backend/services/ProjectTaskService.java
@@ -1,10 +1,12 @@
 package edu.ivanyou.backend.services;
 
+import edu.ivanyou.backend.exception.ProjectIdException;
 import edu.ivanyou.backend.model.Backlog;
 import edu.ivanyou.backend.model.ProjectTask;
 import edu.ivanyou.backend.repositories.BacklogRepository;
 import edu.ivanyou.backend.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +22,9 @@ public class ProjectTaskService {
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
         // PTs should be added to a specific project,(project != null && backlog != null)
         Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        if (backlog == null) {
+            throw new ProjectIdException("Project id: " + projectIdentifier + " does not exist!");
+        }
         // set the backlog to project task
         projectTask.setBacklog(backlog);
         //  // we want our project sequence to be: PROJ1-1 PROJ1-2, PROJ1-3
@@ -45,5 +50,40 @@ public class ProjectTaskService {
 
     public List<ProjectTask> findByProjectIdentifier(String project_id) {
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(project_id);
+    }
+
+    public ProjectTask getProjectTask(String projectIdentifier, String projectSequence) {
+        // 1. make sure that we're searching on an existing backlog(project)
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        if (backlog == null) {
+            throw new ProjectIdException("Project id: " + projectIdentifier + " not found");
+        }
+
+        // 2. make sure that our tasks exists
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(projectSequence);
+        if (projectTask == null) {
+            throw new ProjectIdException("Project Task Sequence: " + projectSequence + " not found");
+        }
+
+        // 3. make sure the project tasks belongs to the project we specify in the path
+        if (!projectIdentifier.equals(projectTask.getProjectIdentifier())) {
+            throw new ProjectIdException("Project Task : " + projectSequence + " does not belong to project: " + projectIdentifier);
+        }
+        return projectTaskRepository.findByProjectSequence(projectSequence);
+    }
+
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask,
+                                             String projectIdentifier,
+                                             String projectSequence) {
+        getProjectTask(projectIdentifier, projectSequence);
+        projectTaskRepository.save(updatedTask);
+        return updatedTask;
+    }
+
+    public ProjectTask deleteByProjectSequence(String projectIdentifier,
+                                        String projectSequence) {
+        ProjectTask projectTask = getProjectTask(projectIdentifier, projectSequence);
+        projectTaskRepository.delete(projectTask);
+        return projectTask;
     }
 }
