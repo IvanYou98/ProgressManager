9911f518fe002646224163ba34e3d24caca7791b
IvanYou98 <yuanfanyou@gmail.com>
2022-03-17 12:52:41 -0400

    FIX THE BUG IN PROJECT BOARD WHEN A PROJECT IS FIRST CREATED

--- front-end/src/components/ProjectBoard/ProjectBoard.js
+++ front-end/src/components/ProjectBoard/ProjectBoard.js
@@ -5,10 +5,11 @@ import {connect} from "react-redux";
 import {loadTasks} from "../../store/backlog";
 
 class ProjectBoard extends Component {
-    projectId = this.props.match.params.id;
+
 
     componentDidMount() {
-        this.props.loadTasks(this.projectId);
+        console.log("LOAD_TASK")
+        this.props.loadTasks(this.props.match.params.id);
     }
 
 
@@ -17,7 +18,7 @@ class ProjectBoard extends Component {
 
         const boardAlgo = (errors, tasks) => {
             if (tasks.length < 1) {
-                if (errors.data.projectIdentifier) {
+                if (errors.data && errors.data.projectIdentifier) {
                     return (
                         <div className="alert alert-danger text-center" role="alert">
                             {errors.data.projectIdentifier}
@@ -36,7 +37,7 @@ class ProjectBoard extends Component {
         }
 
         return (<div className="container">
-                <Link to={`/addProjectTask/${this.projectId}`} className="btn btn-primary mb-3">
+                <Link to={`/addProjectTask/${this.props.match.params.id}`} className="btn btn-primary mb-3">
                     <i className="fas fa-plus-circle"> Create Project Task</i>
                 </Link>
                 {boardAlgo(errors, tasks)}
